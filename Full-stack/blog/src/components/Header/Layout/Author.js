ab6c061157b1cca02c75622a74aa1ec11a826a05
NaftoliSimon <65557591+NaftoliSimon@users.noreply.github.com>
2023-01-06 14:23:20 -0600

    Author link fix

--- Full-stack/blog/src/components/Header/Layout/Author.js
+++ Full-stack/blog/src/components/Header/Layout/Author.js
@@ -3,6 +3,8 @@ import linkedinProfile from '../../../data/linkedinUrl'
 
 export default function Author() {
   return (
-    <a className={`col text-end nav-link pt-2 me-2`}  href={linkedinProfile}>Naftoli Simon</a>
+    <div className='col d-flex flex-row-reverse p-0'>
+      <a className={`text-end nav-link author-link pt-2 me-2`} href={linkedinProfile}>Naftoli Simon</a>
+    </div>
   )
 }
