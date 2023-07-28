1edaa9ab76e089d30dc581c47af925a05f570b9c
NaftoliSimon <65557591+NaftoliSimon@users.noreply.github.com>
2023-07-28 09:22:20 -0500

    show password mobile update

--- Full-stack/blog/src/components/Header&Footer/Login/SignUpModal/Eye.js
+++ Full-stack/blog/src/components/Header&Footer/Login/SignUpModal/Eye.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import React from 'react';
 import { BsEyeFill, BsEyeSlashFill } from 'react-icons/bs';
 
 export default function Eye({ setVisible, visible }) {
-    const show = () => setVisible(true);
-    const hide = () => setVisible(false);
+  const show = () => setVisible(true);
+  const hide = () => setVisible(false);
 
-    const eyeNoSlashIcon = <BsEyeFill size={20} className={`pointer`} onMouseDown={show} onMouseUp={hide} onMouseLeave={hide} />
-    const eyeSlashIcon = <BsEyeSlashFill size={20} className={`pointer`} onMouseDown={show} onMouseUp={hide} onMouseLeave={hide} />
-    return (
-        <div className={`eye-icon pointer`}>
-            <div className={`eye-signUp bg-transparent`}>
-                {visible ? eyeSlashIcon : eyeNoSlashIcon}
-            </div>
-        </div>
-    )
+  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+  const eyeNoSlashIcon = (
+    <BsEyeFill
+      size={20}
+      className={`pointer`}
+      onMouseDown={show}
+      onMouseUp={hide}
+      onMouseLeave={hide}
+      onClick={isMobile ? () => setVisible(!visible) : undefined}
+    />
+  );
+
+  const eyeSlashIcon = (
+    <BsEyeSlashFill
+      size={20}
+      className={`pointer`}
+      onMouseDown={show}
+      onMouseUp={hide}
+      onMouseLeave={hide}
+      onClick={isMobile ? () => setVisible(!visible) : undefined}
+    />
+  );
+
+  return (
+    <div className={`eye-icon pointer`}>
+      <div className={`eye-signUp bg-transparent`}>
+        {visible ? eyeSlashIcon : eyeNoSlashIcon}
+      </div>
+    </div>
+  );
 }
