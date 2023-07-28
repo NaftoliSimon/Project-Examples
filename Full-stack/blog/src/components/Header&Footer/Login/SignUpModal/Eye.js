99d9a0ab76df874e1fd07ddd86e4423f1898d7ac
NaftoliSimon <65557591+NaftoliSimon@users.noreply.github.com>
2023-07-28 09:29:08 -0500

    mobile - show password test update

--- Full-stack/blog/src/components/Header&Footer/Login/SignUpModal/Eye.js
+++ Full-stack/blog/src/components/Header&Footer/Login/SignUpModal/Eye.js
@@ -2,38 +2,38 @@ import React from 'react';
 import { BsEyeFill, BsEyeSlashFill } from 'react-icons/bs';
 
 export default function Eye({ setVisible, visible }) {
-  const show = () => setVisible(true);
-  const hide = () => setVisible(false);
+    const show = () => setVisible(true);
+    const hide = () => setVisible(false);
 
-  const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
-  const eyeNoSlashIcon = (
-    <BsEyeFill
-      size={20}
-      className={`pointer`}
-      onMouseDown={show}
-      onMouseUp={hide}
-      onMouseLeave={hide}
-      onClick={isMobile ? () => setVisible(!visible) : undefined}
-    />
-  );
+    const eyeNoSlashIcon = (
+        <BsEyeFill
+            size={20}
+            className={`pointer`}
+            onMouseDown={show}
+            onMouseUp={hide}
+            onMouseLeave={hide}
+            onClick={isMobile ? () => setVisible(!visible) : undefined}
+        />
+    );
 
-  const eyeSlashIcon = (
-    <BsEyeSlashFill
-      size={20}
-      className={`pointer`}
-      onMouseDown={show}
-      onMouseUp={hide}
-      onMouseLeave={hide}
-      onClick={isMobile ? () => setVisible(!visible) : undefined}
-    />
-  );
+    const eyeSlashIcon = (
+        <BsEyeSlashFill
+            size={20}
+            className={`pointer`}
+            onMouseDown={show}
+            onMouseUp={hide}
+            onMouseLeave={hide}
+            onClick={isMobile ? () => setVisible(!visible) : undefined}
+        />
+    );
 
-  return (
-    <div className={`eye-icon pointer`}>
-      <div className={`eye-signUp bg-transparent`}>
-        {visible ? eyeSlashIcon : eyeNoSlashIcon}
-      </div>
-    </div>
-  );
+    return (<>
+        {isMobile && <div className={`eye-icon pointer`}>
+            <div className={`eye-signUp bg-transparent`}>
+                {visible ? eyeSlashIcon : eyeNoSlashIcon}
+            </div>
+        </div>}
+    </>);
 }
