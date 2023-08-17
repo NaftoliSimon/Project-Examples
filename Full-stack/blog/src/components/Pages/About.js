e114bc1effa1a5b1d2f40520559f51dcaae0d111
NaftoliSimon <65557591+NaftoliSimon@users.noreply.github.com>
2023-08-17 17:01:10 -0500

    about page update

--- Full-stack/blog/src/components/Pages/About.js
+++ Full-stack/blog/src/components/Pages/About.js
@@ -6,49 +6,39 @@ import ExternalLink from '../ExternalLink';
 
 export default function About() {
     //Bootstrap Styles
-    const li = `list-group-item bg-transparent dark border-0 pt-0`;
-    const card = `m-4`;
+    const li = `list-group-item bg-transparent dark border-0 pt-0 text-start`;
+    const h6 = `pb-0 mb-0`;
+    const card = `m-4 text-start`;
+    const pathToAboutComponent = `/src/components/Pages/About.js`;
 
     return (<>
         <div className={`text-center dark opacity-85 about`}>
-            <Card className={card}><Card.Body>
+            <Card className={`m-4`}><Card.Body>
                 <h3>About This Website</h3>
+                <p className='p-0 m-0'>Welcome to my portfolio project, a dynamic full stack blog website that showcases my development skills and passion for creating engaging online experiences.</p>
             </Card.Body></Card>
             <div className='d-block'>
                 <Card className={card}><Card.Body>
-                    <span className='d-block'>
-                        This Project, is a full stack blog website with node.js back-end & CRUD features. I have built my own blog Api that fetched SQL data from my local machine, but am now in the process of updating to AWS server. 
-                    </span>
-                    <span className='d-block'>
-                        This website is built with React, React Router, and Functional Components with hooks.
-                    </span>
-                    <span className='d-block'>
-                        This website was launched with Amazon Web Services - including Route 53, EC2, & more.
-                    </span>
-                    <span className='d-block'>
-                        Was built with a PC & Google Chrome. Tested on phone & tablet (but with no access to the server yet).
-                    </span>
-                    <span className='d-block'>
-                        Was built using Visual Studio Code.
-                    </span>
-                    <strong className='d-block text-uppercase'>
-                        This website is currently under construction.
-                    </strong>
-                    <span className='d-block'>
-                        This website was made by <ExternalLink link={linkedinProfile} text={'Naftoli Simon'} />.
-                    </span>
-                    <span className='d-block'>
-                        Check out the <ExternalLink link={socialMediaLinks.github} text={'Github'} />.
-                    </span>
+                    <p>This project exemplifies my proficiency with cutting-edge technologies. The backend is powered by Node.js, leveraging its versatility and performance capabilities. I've implemented comprehensive CRUD features, ensuring seamless interaction with the data. Initially, I developed a custom blog API to retrieve data from my local SQL database. However, I'm currently in the process of elevating the project by migrating to an AWS server, taking advantage of the scalability and reliability it offers.</p>
+                    <p>On the frontend, I've harnessed the power of React, utilizing both React Router and Functional Components with hooks. This choice reflects my commitment to crafting efficient and maintainable code that delivers a smooth user experience. Throughout the development process, I've prioritized responsiveness and compatibility, meticulously testing the website on various devices including phones and tablets.</p>
+                    <p>The launch of this website was made possible through Amazon Web Services. By leveraging services like Route 53 and EC2, I've ensured a robust and accessible online presence. The collaborative synergy between frontend and backend technologies harmoniously comes to life, offering a seamless and immersive user journey.</p>
+                    <p>I meticulously developed this project using Visual Studio Code, a testament to my dedication to modern and effective development environments. This website is not just the culmination of code and design; it represents a journey of exploration, learning, and creativity.</p>
+                    <p>While this website is currently under construction, it is a reflection of my dedication to growth and innovation. As I continue to enhance this platform, I'm excited to introduce exciting improvements and features. For more details see then Future Updates list below.</p>
+                    <p>Thank you for visiting my portfolio project. I invite you to explore the evolving nature of this website as I strive to create a dynamic and impactful online presence.</p>
+                    <p><span className='d-block'>Best regards,</span>
+                        <span className='d-block'><ExternalLink link={linkedinProfile} text={'Naftoli Simon'} /></span>
+                        <span className='d-block pt-2'><ExternalLink link={socialMediaLinks.github} text={'Check out the Github'} /></span></p>
                 </Card.Body></Card>
+
                 <Card className={card}><Card.Body>
                     <ul className='d-block pt-3 pb-0 list-group list-group-flush'>
-                        <h5><u>FUTURE UPDATES</u></h5>
-                        <li className={li}>Delete features for Blogs and Posts</li>
-                        <li className={li}>Launch Website - (domain & database) </li>
-                        <li className={li}>Add joi validation, display to user limits (ie character length) for ALL input fields</li>
-                        <li className={li}>Fetch and display limited amount of data at one time</li>
-                        <li className={li}>Test on other device types (after launching website with server)</li>
+                        <h5 className='text-center'><u>FUTURE UPDATES</u></h5>
+                        <li className={li}><h6 className={h6}>Delete features for Blogs and Posts</h6> - add the ability to remove/delete both blogs and posts (similar to how comments can already be deleted)</li>
+                        <li className={li}><h6 className={h6}>Launch Website</h6> - launch both the domain & database for people to see the website hosted live and online </li>
+                        <li className={li}><h6 className={h6}>Add joi validation and display to user limits for ALL input fields</h6> - when the user fills out any form sent to the server, certain requirements (such as character length min and max) are set in the server, these requirements need to be displayed to the user</li>
+                        <li className={li}><h6 className={h6}>Fetch and display limited amount of data at one time</h6> - when loading the blogs list, instead of loading all of them, instead fetch and show only x amout at a time</li>
+                        <li className={li}><h6 className={h6}>Test on other device types</h6> - make sure everything looks nice for all device types, including adding both landscape and portrait view (after launching website with server access)</li>
+                        <p className='fst-italic'>There are many more internal updates and improvements not listed here. The ones listed above are the most important ones. For the full list of improvements see the <ExternalLink link={`${socialMediaLinks.github}${pathToAboutComponent}`} text={'Github code'} />.</p>
                     </ul>
                 </Card.Body></Card>
             </div>
@@ -58,7 +48,7 @@ export default function About() {
     )
 }
 /*
-  Coding improvements 
+  Coding improvements - (not everything listed below needs to be updated/improved, they are reminders and suggestions to do if time allows)
 Do all the TODOs throughout the project, or delete them if not applicable
 Improve pre-existing code (use chat GPT)
 Make code easy to read and understand 
